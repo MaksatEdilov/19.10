d95b264aae714a02b1e15e9abc425fc88b1109a8
MaksatEdilov <mikusvol@gmail.com>
2023-10-19 07:22:19 -0700

    Initial Commit

---
+++ 19.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  19.10
+//
+//  Created by Maksat Edil on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("New")
+    }
+
+
+}
+
